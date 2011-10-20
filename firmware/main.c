387a4ef82e03abbe9c97fd3dcc2803f2f1786a68
Ian Daniher <it.daniher@gmail.com>
2011-10-20 14:28:13 -0400

    initial commit of thermowut

--- firmware/main.c
+++ firmware/main.c
@@ -1,201 +1,77 @@
-// Firmware for CEE
-// http://nonolithlabs.com
-// (C) 2011 Kevin Mehall (Nonolith Labs) <km@kevinmehall.net>
 // (C) 2011 Ian Daniher (Nonolith Labs) <ian@nonolithlabs.com>
-// Licensed under the terms of the GNU GPLv3+
 
-#include "cee.h"
-#include "packetbuffer.h"
+#define F_CPU 32000000UL
+#include <avr/io.h>
+#include <avr/delay.h>
 
-unsigned char in_seqno = 0;
+#include "Descriptors.h"
+
+#include "usb.h"
+
+/* Function Prototypes: */
+void configHardware(void);
+void initSPI(void);
+void readSPI(void);
+bool EVENT_USB_Device_ControlRequest(USB_Request_Header_t* req);
 
 int main(void){
 	configHardware();
 	sei();	
-	TCC0.CTRLA = TC_CLKSEL_DIV8_gc; // 4Mhz
-	packetbuf_endpoint_init();
 
-	IN_packet* packet = 0; // device->host packet currently being filled
-	uint8_t sampleIndex = 0; // sample index within the packet
-	
 	while (1){
-		do{
 			USB_Task(); // Lower-priority USB polling, like control requests
-			packetbuf_endpoint_poll();
-		} while (TCC0.CNT < 40); // Wait until it's time for the next packet
-		TCC0.CNT=0;
-
-		if (!packet && packetbuf_in_can_write()){
-			// If we don't have a packet, or the packet is full, get a position to write a new packet
-			packet = (IN_packet *) packetbuf_in_write_position();
-			sampleIndex = 0;
-
-			// Write packet header (stream debugging data)
-			packet->seqno = in_seqno++;
-			packet->reserved[0] = in_count;
-			packet->reserved[1] = out_count;
-
-			// Pretend to consume the data coming from the host at the same speed
-			if (packetbuf_out_can_read()){
-				packetbuf_out_done_read();
-			}
-		}
-		
-		if (packet){
-			readADC(&(packet->data[sampleIndex])); // Write ADC data into the pointer to the IN_sample
-			sampleIndex++;
-
-			if (sampleIndex > 10){ // Packet full
-				packet = 0; // Clear the packet pointer so we get a new one next time.
-				packetbuf_in_done_write(); // Buffer the packet for sending
-			}
-		}
 	}
 }
 
-/* Configures the XMEGA's USARTC1 to talk to the digital-analog converter. */ 
-void initDAC(void){
-	PORTD.DIRSET = 1 << 2; // DAC-SHDN as outputs
-	PORTD.OUTSET = 1 << 2; // DAC-SHDN high
-	PORTC.DIRSET = 1 << 3 | 1 << 4 | 1 << 5 | 1 << 7; //LDAC, CS, SCK, TXD1 as outputs
-	USARTC1.CTRLC = USART_CMODE_MSPI_gc; // SPI master, MSB first, sample on rising clock (UCPHA=0)
-	USARTC1.BAUDCTRLA = 15;  // 1MHz SPI clock. XMEGA AU manual 23.15.6 & 23.3.1
-	USARTC1.BAUDCTRLB =  0;
-	USARTC1.CTRLB = USART_TXEN_bm; // enable TX
-	PORTC.OUTSET = 1 << 3 | 1 << 4; // LDAC, CS high
-	PORTC.OUTCLR = 1 << 5; // SCK low
+/* configure SPI on USARTE0 for communicating with the MAX31855 thermocouple sensor */
+void initSPI(void){
+	PORTE.DIRSET = 1 << 0 | 1 << 1; //CS, SCK as outputs
+	USARTE0.CTRLC = USART_CMODE_MSPI_gc | 1 << 1; // SPI master, MSB first, sample on falling clock (UCPHA=1)
+	USARTE0.BAUDCTRLA = 15;  // 1MHz SPI clock. XMEGA AU manual 23.15.6 & 23.3.1
+	USARTE0.BAUDCTRLB =  0;
+	USARTE0.CTRLB = USART_TXEN_bm | USART_RXEN_bm; // enable TX
+	PORTE.OUTCLR = 1 << 1; // SCK low
+	PORTE.OUTSET = 1 << 0; // CS high
 }
 
-/* Configure the pin modes for the switches and opamps. */
-void initChannels(void){
-	PORTD.DIRSET = 1 << 5 | 1 << 3 | 1 << 1 | 1 << 0; // SHDN-INS-A, SWMODE-A, SHDN-INS-B, EN-OPA-A as outputs
-	PORTC.DIRSET = 1 << 2 | 1 << 0; // SWMODE-B, EN-OPA-B as outputs
-	PORTD.DIRCLR = 1 << 4; // TFLAG-A as input
-	PORTC.DIRCLR = 1 << 1; // TFLAG-B as input
-	PORTB.DIRSET = 1 << 3; // Iset output low
+/* return a 16b value containing the 14b temperature reading */
+void readSPI(void){
+
+	PORTE.OUTCLR = 1 << 0; // CS low
+	_delay_us(10);
+	USARTE0.DATA = 0x00;
+	while(!(USARTE0.STATUS & USART_DREIF_bm)); // wait until we can write another byte
+	USARTE0.STATUS = USART_TXCIF_bm; // clear TX complete flag
+	ep0_buf_in[0] = USARTE0.DATA;
+
+	USARTE0.DATA = 0x00;
+	while(!(USARTE0.STATUS & USART_TXCIF_bm)); // wait for TX complete flag
+	USARTE0.STATUS = USART_TXCIF_bm;
+	ep0_buf_in[1] = USARTE0.DATA;
+	
+	PORTE.OUTSET = 1 << 0; // CS high
 }
 
-/* Configure the shutdown/enable pin states and set the SPTDT switch states. */
-void configChannelA(uint8_t state){
-	switch (state) {
-		case SVMI:
-			PORTD.OUTSET = 1 << 3 | 1 << 0; // SWMODE-A, EN-OPA-A high
-			PORTD.OUTCLR = 1 << 5; // SHDN-INS-A low
-			break;
-		case SIMV:
-			PORTD.OUTSET = 1 << 0; // EN-OPA-A high
-			PORTD.OUTCLR = 1 << 5 | 1 << 3; // SWMODE-A, SHND-INS-A low
-			break;
-		case DISABLED:
-			PORTD.OUTSET = 1 << 3; // SHDN-INS-A high
-			PORTD.OUTCLR = 1 << 5 | 1 << 3; // SWMODE-A, EN-OPA-A low
-			break;
-	}
-}
-
-void configChannelB(uint8_t state){
-	switch (state) {
-		case SVMI:
-			PORTC.OUTSET = 1 << 2 | 1 << 0; // SWMODE-B, EN-OPA-B high
-			PORTD.OUTCLR = 1 << 1; // SHDN-INS-B low
-			break;
-		case SIMV:
-			PORTC.OUTSET = 1 << 0; // EN-OPA-B high
-			PORTC.OUTCLR = 1 << 2; // SWMODE-B low
-			PORTD.OUTCLR = 1 << 1; // SHDN-INS-B low
-			break;
-		case DISABLED:
-			PORTC.OUTCLR = 1 << 2 | 1 << 0; // SWMODE-B, EN-OPA-B low
-			PORTD.OUTSET = 1 << 1; // SHDN-INS-B high
-			break;
-		}
-}
-
-/* Write a value to a specified channel of the DAC with specified flags. */
-void writeDAC(uint8_t flags, uint16_t value){
-	PORTC.OUTCLR = 1 << 4; // CS low
-	USARTC1.DATA = ((flags<<4) & 0xF0) | ((value >> 8) & 0x0F); // munge channel, flags, and four MSB of the value into a single byte
-	while(!(USARTC1.STATUS & USART_DREIF_bm)); // wait until we can write another byte
-	USARTC1.STATUS = USART_TXCIF_bm; // clear TX complete flag
-	USARTC1.DATA = value & 0xFF;
-	while(!(USARTC1.STATUS & USART_TXCIF_bm)); // wait for TX complete flag
-	PORTC.OUTSET = 1 << 4; // CS high
-	PORTC.OUTCLR = 1 << 3; // LDAC low
-	PORTC.OUTSET = 1 << 3; // LDAC high
-}
-
-/* Take a channel, state, and value and configure the switches, shutdowns, and DACs. High level abstraction. */
-void writeChannel(uint8_t channel, uint8_t state, uint16_t value){
-	uint8_t dacflags = 0;
-	if (channel) dacflags |= DACFLAG_CHANNEL;
-	if (state != DISABLED) dacflags |= DACFLAG_ENABLE;
-	if (state == SIMV) dacflags |= DACFLAG_NO_MULT_REF;
-
-	if (channel == 0) configChannelA(state);
-	else              configChannelB(state);
-
-	writeDAC(dacflags, value);
-}
-
-/* Configures the board hardware and chip peripherals for the project's functionality. */
 void configHardware(void){
-	PORTE.DIRSET = 1 << 0 | 1 << 1; // debug LEDs
-	initDAC();
-	initADC();
-	initChannels();
+	initSPI();
 	USB_ConfigureClock();
 	USB_Init();
+	PORTB.DIRSET = 1 << 2;
 }
-
-/* Configure the ADC to 12b, differential w/ gain, signed mode with a 2.5VREF. */
-void initADC(void){
-	ADCA.CTRLB = ADC_RESOLUTION_12BIT_gc | 1 << ADC_CONMODE_bp | 0 << ADC_IMPMODE_bp | ADC_CURRLIMIT_NO_gc | ADC_FREERUN_bm;
-	ADCA.REFCTRL = ADC_REFSEL_AREFA_gc; // use 2.5VREF at AREFA
-	ADCA.PRESCALER = ADC_PRESCALER_DIV32_gc; // ADC CLK = 1MHz
-	ADCA.EVCTRL = ADC_SWEEP_0123_gc ; 
-	ADCA.CH0.CTRL = ADC_CH_INPUTMODE_DIFFWGAIN_gc | ADC_CH_GAIN_1X_gc;
-	ADCA.CH1.CTRL = ADC_CH_INPUTMODE_DIFFWGAIN_gc | ADC_CH_GAIN_1X_gc;
-	ADCA.CH2.CTRL = ADC_CH_INPUTMODE_DIFFWGAIN_gc | ADC_CH_GAIN_1X_gc;
-	ADCA.CH3.CTRL = ADC_CH_INPUTMODE_DIFFWGAIN_gc | ADC_CH_GAIN_1X_gc;
-	ADCA.CH0.MUXCTRL = ADC_CH_MUXNEG_PIN5_gc | ADC_CH_MUXPOS_PIN1_gc; // 1.25VREF vs VS-A
-	ADCA.CH1.MUXCTRL = 0b100 |  ADC_CH_MUXPOS_PIN2_gc; // INTGND vs ADC-A
-	ADCA.CH2.MUXCTRL = 0b100 | ADC_CH_MUXPOS_PIN6_gc; // INTGND vs ADC-B
-	ADCA.CH3.MUXCTRL = ADC_CH_MUXNEG_PIN5_gc | ADC_CH_MUXPOS_PIN7_gc; // 1.25VREF vs VS-B
-	ADCA.CTRLA = ADC_ENABLE_bm;
-}
-
-/* Read the voltage and current from the two channels, pulling the latest samples off "ADCA.CHx.RES" registers. */
-void readADC(IN_sample* s){
-
-	uint8_t offset = 0x16;
-
-	s->a_i = ADCA.CH0.RES; //measure CS-A, monitoring OPA-B
-
-	s->a_v = ADCA.CH1.RES + offset;
-
-	s->b_v = ADCA.CH2.RES + offset;
-
-	s->b_i = ADCA.CH3.RES; // measure CS-B monitoring OPA-A
-}
-
 
 /** Event handler for the library USB Control Request reception event. */
 bool EVENT_USB_Device_ControlRequest(USB_Request_Header_t* req){
 	if ((req->bmRequestType & CONTROL_REQTYPE_TYPE) == REQTYPE_VENDOR){
 		switch(req->bRequest){
-			case 0xA0:
-				readADC((IN_sample *) ep0_buf_in);
-				USB_ep0_send(sizeof(IN_sample));
+			case 0xA0: // read MAX31855
+				readSPI();
+				USB_ep0_send(2);
 				break;
-			case 0xAA:
-				writeChannel(0, req->wIndex, req->wValue);
+			case 0x73: // toggle LED
+				PORTB.OUTTGL = 1 << 2;
 				USB_ep0_send(0);
 				break;
-			case 0xAB:
-				writeChannel(1, req->wIndex, req->wValue);
-				USB_ep0_send(0);
-				break;
-			case 0xBB:
+			case 0xBB: // bootload
 				USB_ep0_send(0);
 				USB_ep0_wait_for_complete();
 				_delay_us(10000);
--- firmware/vendorReqCtrl.py
+++ firmware/vendorReqCtrl.py
@@ -18,43 +18,16 @@ class CEE(object):
 			
 		self.dev.set_configuration()
 
-	def b12unpack(self, s):
-		"Turn a 3-byte string containing 2 12-bit values into two ints"
-		return s[0]|((s[1]&0x0f)<<8), ((s[1]&0xf0) >> 4)|(s[2]<<4)
-		
-	def readADC(self):
-		data = self.dev.ctrl_transfer(0x40|0x80, 0xA0, 0, 0, 6)
-		l = self.b12unpack(data[0:3]) + self.b12unpack(data[3:6])
-		print l
-		vals = map(unpackSign, l)
-		return {
-			'a_v': vals[0]/2048.0*2.5,
-			'a_i': ((vals[1]/2048.0*2.5)-1.25)/45/.07,
-			'b_v': vals[2]/2048.0*2.5,
-			'b_i': ((vals[3]/2048.0*2.5)-1.25)/45/.07,
-		}
+	def getValue(self):
+		data = self.dev.ctrl_transfer(0x40|0x80, 0xA0, 0, 0, 2)
+		data = data[1] << 8 | data[0]
+		print hex(data)
 
-	def set(self, chan, v=None, i=None):
-		cmd = 0xAA+chan
-		if v is not None:
-			dacval = int(round(v/5.0*4095))
-			print dacval
-			self.dev.ctrl_transfer(0x40|0x80, cmd, dacval, MODE_SVMI, 6)
-		elif i is not None:
-			dacval = int((2**12*(1.25+(45*.07*i)))/2.5)
-			print dacval
-			self.dev.ctrl_transfer(0x40|0x80, cmd, dacval, MODE_SIMV, 6)
-		else:
-			self.dev.ctrl_transfer(0x40|0x80, cmd, 0, MODE_DISABLED, 6)	
-
-	def setA(self, v=None, i=None):
-		self.set(0, v, i)
-	
-	def setB(self, v=None, i=None):
-		self.set(1, v, i)
-
-	def bootload(self):
-		self.dev.ctrl_transfer(0x40|0x80, 0xBB, 0, 0, 1)
+	def blinkLED(self):
+		self.dev.ctrl_transfer(0x40|0x80, 0x73, 0, 0, 0)	
 
 if __name__ == "__main__":
 	cee = CEE()
+	while True:
+		cee.blinkLED()
+		cee.getValue()
